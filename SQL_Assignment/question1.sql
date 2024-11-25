0f675e936af9a492e74181bac22c9d3018a7899f
delicious10 <abenadollie@gmail.com>
2024-11-25 07:03:40 -0800

    Added question1.sql

---
+++ SQL_Assignment/question1.sql
@@ -0,0 +1,22 @@
+/*Create the database*/ 
+CREATE TABLE IF NOT EXISTS Tech4Girls_DB;
+SHOW DATABASES;
+USE Tech4Girls_DB;
+
+
+/* creating a table called user */
+CREATE TABLE IF NOT EXISTS User (
+    id Primary Key Auto,
+    Username VARCHAR(50),
+    email VARCHAR(100),
+    created_at TIMESTAMP CURRENT_TIMESTAMP
+);
+
+SHOW TABLES;
+
+/* Insert sample data into the table */
+INSERT INTO Users (username, email, created_at)
+VALUES 
+('ama', 'ama@example.com',;2024-11-01 10:30:00),
+('Abena', 'abena@example.com', 2024-11-02 12:00:00),
+('adjoa', 'adjoa@example.com', 2024-11-03 14:15:00);
